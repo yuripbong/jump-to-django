4d0dcf73fb5fc5ef0739ec175d674cd7ed3ac239
yuripbong <kyri200350@gmail.com>
2025-01-03 18:17:18 +0900

    feat: 2-10 폼

--- pybo/views.py
+++ pybo/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Question, Answer
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -18,6 +20,28 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    answer.save()
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        return HttpResponseNotAllowed(['Only POST is possible.'])
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid(): # 폼이 유효하면
+            question = form.save(commit=False) # 임시 저장하여 question 객체 리턴
+            question.create_date = timezone.now() # 실제 저장을 위해 작성일시 설정
+            question.save() # 데이터 저장
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
